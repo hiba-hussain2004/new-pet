09b805034dc39ab705163b0bb48774379cf15f2a
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-15 00:51:58 +0530

    navigation done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
+import Addpet from './components/Addpet'
+import ViewPet from './components/ViewPet'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Addpet />} />
+        <Route path="/view" element={<ViewPet />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
